0f74a3227f651afc7a4528aff5936f2d5fe23074
ggonza55 <ggonza55@uic.edu>
2023-03-03 13:22:33 -0600

    Modified big activity

--- app/src/main/java/edu/uic/cs478/s2023/projecttwo/BigImageActivity.java
+++ app/src/main/java/edu/uic/cs478/s2023/projecttwo/BigImageActivity.java
@@ -1,10 +1,26 @@
 package edu.uic.cs478.s2023.projecttwo;
 
-import android.content.Intent;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BigImageActivity extends AppCompatActivity {
-    Intent i = this.getIntent();
+import android.content.Intent;
+import android.os.Bundle;
 
+import java.util.ArrayList;
+
+public class BigImageActivity extends AppCompatActivity {
+
+    String[] imgList = {"img_a","img_b",
+                        "img_c","img_d",
+                        "img_e","img_f",
+                        "img_g","img_h"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_big_image);
+
+
+        Intent i = this.getIntent();
+        int ndx = i.getIntExtra("carIndex", -1);
+    }
 }
