28d6d37968e863a812407483d37eea17e5cfc2bc
GGMachina <ggonza55@uic.edu>
2023-03-03 19:02:17 -0600

    finished short click and chose cars

--- app/src/main/java/edu/uic/cs478/s2023/projecttwo/BigImageActivity.java
+++ app/src/main/java/edu/uic/cs478/s2023/projecttwo/BigImageActivity.java
@@ -3,16 +3,24 @@ package edu.uic.cs478.s2023.projecttwo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class BigImageActivity extends AppCompatActivity {
 
-    String[] imgList = {"img_a","img_b",
-                        "img_c","img_d",
-                        "img_e","img_f",
-                        "img_g","img_h"};
+    int[] imgList = {R.drawable.kona_n_hd, R.drawable.charger_hd,
+                    R.drawable.mg5_hd, R.drawable.spectre_hd,
+                    R.drawable.civic_type_r_hd, R.drawable.sierra_hd,
+                    R.drawable.mustang_hd,R.drawable.porsche_hd,};
+    int[] sites = {R.string.site_0, R.string.site_1, R.string.site_2
+                ,R.string.site_3, R.string.site_4, R.string.site_5
+                ,R.string.site_6,R.string.site_7,R.string.site_8};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,26 @@ public class BigImageActivity extends AppCompatActivity {
 
         Intent i = this.getIntent();
         int ndx = i.getIntExtra("carIndex", -1);
+        String name = i.getStringExtra("carName");
+        setPage(ndx, name);
+    }
+
+    private void setPage(int index, String title) {
+        if (index == -1) {
+            throw new RuntimeException("Tried to set page but got no index...");
+        }
+
+        ImageView bigImg = findViewById(R.id.bigImage);
+        TextView bigTxt = (TextView) findViewById(R.id.bigText);
+
+        bigImg.setImageResource(imgList[index]);
+        bigTxt.setText(title);
+        bigImg.setOnClickListener(view -> {
+            Toast.makeText(BigImageActivity.this, "Website for this item is: " + getResources().getString(sites[index]), Toast.LENGTH_SHORT).show();
+//            DONE: open site in browser
+            Intent browseIntent = new Intent(Intent.ACTION_VIEW);
+            browseIntent.setData(Uri.parse(getResources().getString(sites[index])));
+            startActivity(browseIntent);
+        });
     }
 }
--- app/src/main/java/edu/uic/cs478/s2023/projecttwo/MainActivity.java
+++ app/src/main/java/edu/uic/cs478/s2023/projecttwo/MainActivity.java
@@ -24,26 +24,26 @@ public class MainActivity extends AppCompatActivity {
         gridView = findViewById(R.id.mainGrid);
         ArrayList<CardModel> cardModelArrayList = new ArrayList<>();
 
-//        TODO: Give Custom Titles & Images of cars
-        cardModelArrayList.add(new CardModel("Kona N.", R.drawable.kona_n));
-        cardModelArrayList.add(new CardModel("Title 2", R.drawable.img_b));
-        cardModelArrayList.add(new CardModel("Title 3", R.drawable.img_c));
-        cardModelArrayList.add(new CardModel("Title 4", R.drawable.img_d));
-        cardModelArrayList.add(new CardModel("Title 5", R.drawable.img_e));
-        cardModelArrayList.add(new CardModel("Title 6", R.drawable.img_f));
-        cardModelArrayList.add(new CardModel("Title 7", R.drawable.img_g));
-        cardModelArrayList.add(new CardModel("Title 8", R.drawable.img_h));
+//        DONE: Give Custom Titles & Images of cars
+        cardModelArrayList.add(new CardModel("Hyundai Kona N.", R.drawable.kona_n));
+        cardModelArrayList.add(new CardModel("Dodge Charger", R.drawable.charger));
+        cardModelArrayList.add(new CardModel("Saic MG5", R.drawable.mg5));
+        cardModelArrayList.add(new CardModel("Rolls-Royce Spectre", R.drawable.spectre));
+        cardModelArrayList.add(new CardModel("Civic Type R", R.drawable.civic_type_r));
+        cardModelArrayList.add(new CardModel("GMC Sierra HD Denali", R.drawable.sierra));
+        cardModelArrayList.add(new CardModel("Ford Mustang", R.drawable.mustang));
+        cardModelArrayList.add(new CardModel("Volkswagen Porsche", R.drawable.porsche));
 
         GridAdapter adapter = new GridAdapter(this, cardModelArrayList);
         gridView.setAdapter(adapter);
 
         gridView.setOnItemClickListener((adapterView, view, i, l) -> {
-//                TODO: Implement short Click
+//                DONE: Implement short Click
             TextView tv = (TextView) view.findViewById(R.id.txtView);
             Intent bigIntent = new Intent(MainActivity.this, BigImageActivity.class);
             bigIntent.putExtra("carIndex", i);
+            bigIntent.putExtra("carName", tv.getText().toString());
             startActivity(bigIntent);
-
 
             Log.d("ClickedGridItem", "text: "+tv.getText().toString() + " i: " + i + " l: " + l  );
         });
